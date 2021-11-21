1ffc6d4a038434934b6fe834c5da2820b5ca84a7
Irene <irenesis123@gmail.com>
2021-11-20 20:19:54 -0500

    fundamentals

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import { StyledHeader } from './styles/Header.styled';
+
+export default function Header() {
+  return (
+    <StyledHeader bg="red">
+      <h1>hubble</h1>
+    </StyledHeader>
+  );
+}
